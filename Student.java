9ec2376e6530213828686eba1fa2d98e8cefb4df
dappilisunilkumar <dappilisunil@gmail.com>
2025-07-02 11:08:54 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+	int age=21;
+	String name="sunil";
+	boolean isGraduated=false;
+	public static void main(String[] args) {
+		Student student=new Student();
+		System.out.println( student.age); 
+		System.out.println( student.name);
+		System.out.println( student.isGraduated);
+	
+
+	
+		}
+	
+	}
+	
+
